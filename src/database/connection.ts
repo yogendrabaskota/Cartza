0a630f4be0f7f52cd6b06b4a9d7300e6689a01f4
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-11-13 21:41:54 +0545

    connect

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -9,7 +9,7 @@ const sequelize = new Sequelize({
     password : process.env.DB_PASSWORD,
     
     host : process.env.DB_HOST,
-    port : Number(process.env.DB_PORT),
+    port : Number(process.env.DB_PORT), 
     models : [__dirname + "/models"]
 
 })
