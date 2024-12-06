0eb7f641942e7bc85af9b6a80fc0b68b83dfb5eb
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-12-06 21:49:20 +0545

    payment table added

---
+++ src/database/models/Payment.ts
@@ -0,0 +1,54 @@
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    CreatedAt,
+    PrimaryKey,
+    AllowNull
+    
+} from 'sequelize-typescript'
+
+@Table({
+    tableName : 'orders', 
+    modelName : 'Order', 
+    timestamps : true
+})
+
+class Order extends Model{
+    @Column({
+        primaryKey : true,
+        type : DataType.UUID,
+        defaultValue : DataType.UUIDV4,
+
+
+    })
+    declare id : string
+
+    @Column({
+        type : DataType.ENUM('cod','khalti','esewa'),
+        allowNull : false
+        
+    })
+    declare paymentMethod : string
+
+    @Column({
+        type : DataType.ENUM('paid','unpaid'),
+        defaultValue : 'unpaid'
+        
+    })
+    declare paymentStatus : string
+
+    @Column({
+        type : DataType.STRING
+       
+        
+    })
+    declare pidx : string
+
+
+
+    
+}
+
+export default Order
