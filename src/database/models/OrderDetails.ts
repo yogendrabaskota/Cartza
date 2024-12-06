daf2d8dc0bb90cc168ada4d368d7ede5a482d945
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-12-06 21:44:52 +0545

    orderDetail table created

---
+++ src/database/models/OrderDetails.ts
@@ -0,0 +1,37 @@
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    CreatedAt,
+    PrimaryKey,
+    AllowNull
+    
+} from 'sequelize-typescript'
+
+@Table({
+    tableName : 'orderdetails', 
+    modelName : 'OrderDetail', 
+    timestamps : true
+})
+
+class OrderDetail extends Model{
+    @Column({
+        primaryKey : true,
+        type : DataType.UUID,
+        defaultValue : DataType.UUIDV4,
+
+
+    })
+    declare id : string
+
+    @Column({
+        type : DataType.INTEGER,
+        allowNull : false
+        
+    })
+    declare quantity : number
+    
+}
+
+export default OrderDetail
