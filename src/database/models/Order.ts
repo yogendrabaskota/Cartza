e630ece46d677564d1ded52fba9bb387e5b302e3
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-12-06 21:39:44 +0545

    order table created

---
+++ src/database/models/Order.ts
@@ -0,0 +1,62 @@
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    CreatedAt,
+    PrimaryKey,
+    AllowNull
+    
+} from 'sequelize-typescript'
+
+@Table({
+    tableName : 'orders', 
+    modelName : 'Order', 
+    timestamps : true
+})
+
+class Order extends Model{
+    @Column({
+        primaryKey : true,
+        type : DataType.UUID,
+        defaultValue : DataType.UUIDV4,
+
+
+    })
+    declare id : string
+
+    @Column({
+        type : DataType.STRING,
+        allowNull : false
+        
+    })
+    declare phoneNumber : string
+
+    @Column({
+        type : DataType.STRING,
+        allowNull : false
+        
+    })
+    declare shippingAddress : string
+
+    @Column({
+        type : DataType.FLOAT,
+        allowNull : false
+        
+    })
+    declare totalAmount : number
+
+    @Column({
+        type : DataType.ENUM('pending','cancelled','delivered','ontheway','preparation'),
+        defaultValue : 'pending',
+        allowNull : false
+        
+    })
+    declare orderStatus : string
+
+    
+
+    
+}
+
+export default Order
