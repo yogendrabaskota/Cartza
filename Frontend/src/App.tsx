c2b95ec0e3f81ac474cb7821fa059dc364e08458
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-06-14 19:38:50 +0545

    connect react with redux

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,19 +1,24 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
+import { Provider } from 'react-redux'
 import './App.css'
 import Home from './components/Home'
+import store from './store/store'
 
 function App() {
 
 
   return (
-  
+
+    <Provider store = {store}>
       <BrowserRouter>
       <Routes>
         <Route path='/home' element ={<Home />} /> 
 
       </Routes>
       </BrowserRouter>
+    </Provider>
+  
+
     
   )
 }
