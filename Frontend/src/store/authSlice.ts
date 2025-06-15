a7c8476863ce67de612589ff64d52652173fbe96
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-06-15 16:32:37 +0545

    auth thunk created

--- Frontend/src/store/authSlice.ts
+++ Frontend/src/store/authSlice.ts
@@ -1,6 +1,18 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import {createSlice, type PayloadAction} from '@reduxjs/toolkit'
+import axios from 'axios'
 
+interface RegisterData{
+    username : string,
+    email : string,
+    password : string
+}
+interface LoginData{
+    email : string,
+    password : string
+}
 
 interface User {
     username : string,
@@ -9,15 +21,22 @@ interface User {
     token : string,
     
 }
+// enum STASTUS {
+//     loading, //= 'Loading',
+//     success, //= 'Success',
+//     error, // = 'Error'
+// }
 interface AuthState {
     user : User,
     status : string
 }
 
+
 const initialState: AuthState = {
     user : {} as User,
-    status : 'loading'
+    status : ''
 }
+
 
 const authSlice = createSlice({
     name : 'auth',
@@ -34,3 +53,44 @@ const authSlice = createSlice({
 
 export const {setUser,setStatus} = authSlice.actions
 export default authSlice.reducer
+
+
+function register(data:RegisterData){
+    return async function registerThunk(dispatch:any){
+        dispatch(setStatus('loading'))
+       try {
+         const response = await axios.post('http://localhost:3000/register',data)
+        if(response.status === 201){
+            dispatch(setStatus('success'))
+        }else{
+            dispatch(setStatus('error'))
+
+            
+        }
+        
+       } catch (error) {
+        dispatch(setStatus('error'))
+        
+       }
+    }
+}
+
+function login(data:LoginData){
+    return async function loginThunk(dispatch:any){
+        dispatch(setStatus('loading'))
+        try {
+            const response = await axios.post('http://localhost:3000/register',data)
+            if(response.status === 200){
+                dispatch(setStatus('success'))
+
+            }else{
+                dispatch(setStatus('error'))
+
+            }
+        } catch (error) {
+            dispatch(setStatus('error'))
+            
+        }
+
+    }
+}
